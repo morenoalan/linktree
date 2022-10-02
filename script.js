7c92eb7015242492d85b65368ba6d4a9adc2d360
Alan Moreno <alan.moreno@hotmail.com>
2022-10-01 23:38:35 -0300

    updating icons, starting script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function GrabVectors() {
+    allVectors = document.getElementsByClassName('icon')[0].childNodes[1].getAttribute('id');
+    console.log(allVectors);
+    /*
+    for(let i = 0; i < allVectors.length; i++) {
+
+    }
+
+    */
+}
+GrabVectors();
