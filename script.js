bd93289615d3f4f043a80e35128d71ffe93b6f9c
Alan Moreno <alan.moreno@hotmail.com>
2022-10-03 22:30:57 -0300

    GrabVectors() failing load element

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function GrabVectors() {
-    let allVectors = document.getElementsByClassName('icon')[0].childNodes[1].getAttribute('id');
-    console.log(allVectors);
-    let  vectorFinder = allVectors.slice(5, allVectors.length);
-    vectorFinder = 'material-filled-' + vectorFinder;
-    console.log(vectorFinder);
+    let whatIconIWillPut = document.getElementsByClassName('icon')[0].childNodes[1].getAttribute('id');
+    console.log(whatIconIWillPut);
+    let namingVector = whatIconIWillPut.slice(5, whatIconIWillPut.length);
+    namingVector = 'material-filled-' + namingVector;
+    console.log(namingVector);
+    let iframeAddress = 'https://vectours.blogspot.com';
+    console.log(iframeAddress.contentDocument.getElementById(namingVector));
 }
 GrabVectors();
