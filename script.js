85dfb4a9d240849ccf97c3d8bc791cd67899ee36
Alan Moreno <alan.moreno@hotmail.com>
2022-11-11 20:57:48 -0300

    updated html and js; divided css

--- script.js
+++ script.js
@@ -27,26 +27,3 @@ function ChangeTheme(element) {
         document.getElementById('favicon').setAttribute('href', avatarBlackWhite);
     }
 }
-
-/*
-function GrabVectors() {
-    let whatIconIWillPut = document.getElementsByClassName('icon')[0].childNodes[1].getAttribute('id');
-    console.log(whatIconIWillPut);
-    let namingVector = whatIconIWillPut.slice(5, whatIconIWillPut.length);
-    namingVector = 'material-filled-' + namingVector;
-    console.log(namingVector);
-    let iframeAddress = 'https://vectours.blogspot.com';
-    console.log(iframeAddress);
-    //cross-origin problems
-}
-GrabVectors();
-
-var statusLanguage;
-
-function changeLanguage(statusLanguage){
-    let pTagsList = document.getElementsByClassName('p');
-    for (let i; i < pTagsList.length; i++){
-        pTagsList[i].outerText = '';
-    }
-}
-*/
