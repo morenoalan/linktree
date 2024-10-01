b229ccae0aaedc7ee5aea432247c575d1ed5ed36
Alan Moreno <alan.moreno@hotmail.com>
2024-09-30 23:16:36 -0300

    update changeLanguage

--- main.js
+++ main.js
@@ -27,3 +27,25 @@ function ChangeTheme(element) {
         document.getElementById('favicon').setAttribute('href', avatarBlackWhite);
     }
 }
+
+function changeLanguage(lang) {
+    let allLangs = [];
+    document.querySelectorAll('.bt_lang').forEach(function(el) {
+        allLangs.push(el.id.slice(3));
+    });
+    console.log(allLangs);
+    allLangs.forEach(function(el) {
+        el2 = '.'+el;
+        console.log(el2);
+        if(el == lang) {
+            document.querySelectorAll(`${el2}`).forEach(function(elx) {
+                elx.classList.remove('display-none');
+            });
+        } else {
+            document.querySelectorAll(`${el2}`).forEach(function(elx) {
+                elx.classList.add('display-none');
+            });
+        }
+    })
+
+}
